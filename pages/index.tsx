57dc1ce1a6d8dd0089c58777941949b50a9c8ffb
Carlos Costa <carllos.nc@gmail.com>
2021-06-09 12:28:21 -0300

    adding page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { Welcome } from '../components'
 import { Page } from '../templates'
+import Head from "next/head"
 
 export default function Home(){
   return (
     <Page>
+      <Head>
+        <title> CN Next Starter </title>
+      </Head>
       <Welcome />
     </Page>
   )

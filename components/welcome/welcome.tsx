eb6e308b27a66683da4a638547de3ac7a7bb25e3
Carlos Costa <carllos.nc@gmail.com>
2021-06-09 11:35:46 -0300

    initial commit

---
+++ components/welcome/welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './welcome.module.scss'
+
+export function Welcome(){
+  return (
+    <div className={style.welcome}>
+      <h1 className={style.title}> CN Next starter </h1>
+      <p className={style.description}> Stater project with Next and TypeScript. </p>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Welcome } from '../components'
+import { Page } from '../templates'
+
+export default function Home(){
+  return (
+    <Page>
+      <Welcome />
+    </Page>
+  )
+}

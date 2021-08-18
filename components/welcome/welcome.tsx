2b9c266346f72e1d918f9f5c0bf5511c525852c7
Carlos Costa <carllos.nc@gmail.com>
2021-08-18 08:49:14 -0300

    Adding env support

--- components/welcome/welcome.tsx
+++ components/welcome/welcome.tsx
@@ -6,6 +6,8 @@ export function Welcome(){
     <div className={style.welcome}>
       <h1 className={style.title}> CN Next starter </h1>
       <p className={style.description}> Stater project with Next and TypeScript. </p>
+
+      { process.env.NEXT_PUBLIC_VERSION }
     </div>
   )
 }

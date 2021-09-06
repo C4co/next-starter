eb4b495100ffd2c9ff3460f78235810230116baa
Carlos Costa <carllos.nc@gmail.com>
2021-09-06 11:37:24 -0300

    Ignoring cypress

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,11 @@ module.exports = {
     '^@/components/(.*)$': '<rootDir>/components/$1'
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+    '<rootDir>/cypress/'
+  ],
   testEnvironment: 'jsdom',
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset

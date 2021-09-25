6d4edf0ecaf868003b28b687108c12eeaa982b72
Carlos Costa <carllos.nc@gmail.com>
2021-09-25 01:53:08 -0300

    Small refacts

--- components/welcome/welcome.tsx
+++ components/welcome/welcome.tsx
@@ -18,8 +18,13 @@ export function Welcome(){
       <h1 data-testid="title" className={style.title}> CN Next starter </h1>
       <p data-testid="description" className={style.description}> { t('description') } </p>
       <span data-testid="version"> { process.env.NEXT_PUBLIC_VERSION } </span>
-      <button onClick={ptbr}> pt_BR </button>
-      <button onClick={en}> en </button>
+
+      <br />
+
+      <div className={style.buttons}>
+        <button className={style.button} onClick={ptbr}> pt_BR </button>
+        <button className={style.button} onClick={en}> en </button>
+      </div>
     </div>
   )
 }

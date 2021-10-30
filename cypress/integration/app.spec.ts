1c11ae6fc64bbbf1c823cd6d2dbf0baceda3c49b
Carlos Costa <carllos.nc@gmail.com>
2021-10-30 18:00:53 -0300

    Reconfig cypress.

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,6 +1,6 @@
 describe('navigation', () => {
   it('Check home page', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
     cy.contains('CN Next starter')
     cy.contains('Stater project with Next and TypeScript.')
     cy.contains('Version: 0.0.1')

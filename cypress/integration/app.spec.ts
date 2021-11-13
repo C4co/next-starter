2d01836ce1254743e9b79dcda596f60fcaadb14e
Carlos Costa <carllos.nc@gmail.com>
2021-11-13 02:07:46 -0300

    Fixing home page integration test.

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,9 +1,8 @@
 describe('navigation', () => {
   it('Check home page', () => {
     cy.visit('/')
-    cy.contains('CN Next starter')
-    cy.contains('Stater project with Next and TypeScript.')
-    cy.contains('Version: 0.0.1')
+    cy.contains('loading...')
+    cy.contains('home page')
   })
 })
 

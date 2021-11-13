6bf1dc30f1d11397d630f330c3f12548a5a81f81
Carlos Costa <carllos.nc@gmail.com>
2021-11-13 02:08:20 -0300

    New home page structure.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
-import { Welcome } from '@/components'
 import { Page } from '@/templates'
+import { useLoremIpsum } from '@/services'
 
 export default function Home(){
+  const { data, isError, isLoading } = useLoremIpsum()
+
+  if(isLoading){
+    return (
+      <Page>
+        <h1> loading... </h1>
+       </Page>
+    )
+  }
+
+  if(isError){
+    return (
+      <Page>
+        <h1> error </h1>
+      </Page>
+    )
+  }
+
+  console.log(data)
+
   return (
     <Page>
-      <Welcome />
+      <h1> home page </h1>
     </Page>
   )
 }

1a18bde8fac51443ea3143e64364b47297d6265e
Carlos Costa <carllos.nc@gmail.com>
2022-05-29 19:59:34 -0300

    chore: move all main content to ./src dir

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,6 +4,6 @@ import '@testing-library/jest-dom/extend-expect'
 // setup i18n
 import { initReactI18next } from 'react-i18next'
 import i18n from 'i18next'
-import { CONTENT } from './content'
+import { CONTENT } from './src/content'
 
 i18n.use(initReactI18next).init(CONTENT)
